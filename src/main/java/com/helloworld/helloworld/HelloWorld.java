22ecee0aff261e138a735aa152bb6043d384a39d
e99999 <26906600+e99999@users.noreply.github.com>
2018-10-19 14:31:49 -0700

    proxy stuff
    proxy stuff

--- src/main/java/com/helloworld/helloworld/HelloWorld.java
+++ src/main/java/com/helloworld/helloworld/HelloWorld.java
@@ -1,31 +1,44 @@
 package com.helloworld.helloworld;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = HelloWorld.MODID, name = HelloWorld.NAME, version = HelloWorld.VERSION)
-public class HelloWorld
-{
+import com.helloworld.proxy.CommonProxy;
+
+@Mod(modid = HelloWorld.MODID, name = HelloWorld.MODNAME, version = HelloWorld.MODVERSION, useMetadata = true)
+public class HelloWorld {
+
     public static final String MODID = "helloworld";
-    public static final String NAME = "Hello World";
-    public static final String VERSION = "1.0";
+    public static final String MODNAME = "Hello World";
+    public static final String MODVERSION= "0.0.1";
 
-    private static Logger logger;
+    @SidedProxy(clientSide = "com.helloworld.proxy.ClientProxy", serverSide = "com.helloworld.proxy.ServerProxy")
+    public static CommonProxy proxy;
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    @Mod.Instance
+    public static HelloWorld instance;
+
+    public static Logger logger;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
+        proxy.preInit(event);
     }
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some hello world action
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent e) {
+        proxy.init(e);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent e) {
+    	// some hello world action
         logger.info("HELLO WORLD!");
+    	proxy.postInit(e);
     }
 }
---
+++ src/main/java/com/helloworld/proxy/ServerProxy.java
@@ -0,0 +1,5 @@
+package com.helloworld.proxy;
+
+public class ServerProxy extends CommonProxy {
+
+}
