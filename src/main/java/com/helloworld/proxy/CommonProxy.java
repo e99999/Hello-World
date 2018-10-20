8753afd9bfb81e1f4a8ce337c627ea66e05a4265
e99999 <26906600+e99999@users.noreply.github.com>
2018-10-19 17:22:30 -0700

    config generation and grass and water : )
    simple config generation, and overiding textures for water and foliage that will be configurable

--- src/main/java/com/helloworld/proxy/CommonProxy.java
+++ src/main/java/com/helloworld/proxy/CommonProxy.java
@@ -1,7 +1,10 @@
 package com.helloworld.proxy;
 
+import com.helloworld.helloworld.Config;
+
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -9,15 +12,28 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.io.File;
+
 @Mod.EventBusSubscriber
 public class CommonProxy {
+
+    // Config instance
+    public static Configuration config;
+
     public void preInit(FMLPreInitializationEvent e) {
+        File directory = e.getModConfigurationDirectory();
+        config = new Configuration(new File(directory.getPath(), "helloworld.cfg"));
+        Config.readConfig();
     }
+
 
     public void init(FMLInitializationEvent e) {
     }
 
     public void postInit(FMLPostInitializationEvent e) {
+        if (config.hasChanged()) {
+            config.save();
+        }
     }
 
     @SubscribeEvent
