586bfaf89c770816b98c0e69f377a670f410e113
e99999 <26906600+e99999@users.noreply.github.com>
2018-10-19 20:45:48 -0700

    fixed naming of item

--- src/main/java/com/helloworld/helloworld/items/FirstItem.java
+++ src/main/java/com/helloworld/helloworld/items/FirstItem.java
@@ -12,7 +12,7 @@ public class FirstItem extends Item {
 
     public FirstItem() {
         setRegistryName("firstitem");        // The unique name (within your mod) that identifies this item
-        setUnlocalizedName("First Item");     // Used for localization (en_US.lang)
+        setUnlocalizedName(HelloWorld.MODID + ".firstitem");     // Used for localization (en_US.lang)
     }
     
     @SideOnly(Side.CLIENT)
